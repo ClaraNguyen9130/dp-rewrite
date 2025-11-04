bada7d9839f791d3f3b87068333e03eeda61fc14
Clara Nguyen <54362386+ClaraNguyen9130@users.noreply.github.com>
2025-11-04 17:14:29 +0000

    update script

---
+++ sample_scripts/atis_rewrite_experiment.sh
@@ -0,0 +1,85 @@
+# Epsilon 1
+python main.py \
+  --mode rewrite \
+  --model adept \
+  --dataset atis \
+  --experiment adept_l2norm_rewrite \
+  --name rewrite_atis_e1 \
+  --output_dir results/ \
+  --asset_dir assets/ \
+  --last_checkpoint_path results/pretrain_atis/checkpoints/checkpoint.pt \
+  --embed_type glove \
+  --embed_dir_unprocessed assets/embeds/glove.6B.100d.txt \
+  --embed_size 100 \
+  --epochs 1 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --early_stopping True \
+  --patience 1 \
+  --train_teacher_forcing_ratio 0.5 \
+  --clipping_constant 1.0 \
+  --l_norm 2 \
+  --private True \
+  --epsilon 1 \
+  --delta 1e-5 \
+  --dp_mechanism gaussian \
+  --dp_module decoder \
+  --seed 42
+
+# Epsilon 100
+
+python main.py \
+  --mode rewrite \
+  --model adept \
+  --dataset atis \
+  --experiment adept_l2norm_rewrite \
+  --name rewrite_atis_e100 \
+  --output_dir results/ \
+  --asset_dir assets/ \
+  --last_checkpoint_path results/pretrain_atis/checkpoints/checkpoint.pt \
+  --embed_type glove \
+  --embed_dir_unprocessed assets/embeds/glove.6B.100d.txt \
+  --embed_size 100 \
+  --epochs 1 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --early_stopping True \
+  --patience 1 \
+  --train_teacher_forcing_ratio 0.5 \
+  --clipping_constant 1.0 \
+  --l_norm 2 \
+  --private True \
+  --epsilon 100 \
+  --delta 1e-5 \
+  --dp_mechanism gaussian \
+  --dp_module decoder \
+  --seed 42
+
+  # Epsilon 1000
+
+python main.py \
+  --mode rewrite \
+  --model adept \
+  --dataset atis \
+  --experiment adept_l2norm_rewrite \
+  --name rewrite_atis_e1000 \
+  --output_dir results/ \
+  --asset_dir assets/ \
+  --last_checkpoint_path results/pretrain_atis/checkpoints/checkpoint.pt \
+  --embed_type glove \
+  --embed_dir_unprocessed assets/embeds/glove.6B.100d.txt \
+  --embed_size 100 \
+  --epochs 1 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
+  --early_stopping True \
+  --patience 1 \
+  --train_teacher_forcing_ratio 0.5 \
+  --clipping_constant 1.0 \
+  --l_norm 2 \
+  --private True \
+  --epsilon 1000 \
+  --delta 1e-5 \
+  --dp_mechanism gaussian \
+  --dp_module decoder \
+  --seed 42
